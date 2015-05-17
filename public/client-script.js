f4722a27ea544ce4fa49b02b830352c49dda92d7
Charanjit Singh <ckhabra007@gmail.com>
2015-05-17 13:50:33 +0530

    Implement proper dispatcher

--- public/client-script.js
+++ public/client-script.js
@@ -15,10 +15,53 @@
 
   function putSocketsToWork() {
     var socket = io(baseUrl);
+    checkForLiveUpdate();
 
     socket.on("push code", function(doc) {
+      if (! checkForLiveUpdate())
+        return;
+
       console.warn("Hot Pushing file", doc.filepath);
+      var LiveUpdate = window.LiveUpdate;
+
+      var filepath = doc.filepath,
+          filetype = doc.filetype,
+          content = doc.content,
+          oldContent = doc.oldContent,
+          dontEval = doc.dontEval,
+          prePush = doc.prePush;
+
+      if (!filetype || !content) {
+        console.error('Invalid doc for Dispatching to LiveUpdate');
+        return;
+      }
+
+      if (dontEval) {
+        if (prePush) {
+          return;
+        }
+
+        console.warn("First time pushing file, has to reload. It'll be hot-pushed for any further changes");
+        LiveUpdate.forceRefreshPage();
+        return;
+      }
+
+      console.warn("Hot pushing ", filepath);
+      LiveUpdate.refreshFile({
+        fileType: filetype,
+        newContent: content,
+        oldContent: oldContent
+      });
+
     });
+  }
+
+  function checkForLiveUpdate() {
+    if (typeof window.LiveUpdate === 'undefined') {
+      console.warn("You don't have LiveUpdate installed. Please install it with `meteor add nucleuside:live-update` for hot-push");
+      return false;
+    }
+    return true;
   }
 
 
